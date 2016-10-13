3faf7695163be606042268fa96bff9f19a9d161f
Andreas Wiedemann <andreas.wiedemann@mail.de>
2016-10-14 00:01:21 +0200

    Script to find missing label names and set it

--- ColorPalette/displayImages.py
+++ ColorPalette/displayImages.py
@@ -1,62 +1,111 @@
-from PIL import Image
+from PIL import Image, ImageTk
 import glob, os
 import numpy as np
 import pandas as pd
+import sys
+import string
 
-colormap = np.loadtxt("LabelColorMapping.csv",skiprows=1,delimiter=';',usecols=(1,2,3))
-colormap = colormap.astype(int)
+import tkinter
+from tkinter import simpledialog
 
-csv_file = pd.read_csv("LabelColorMapping.csv",delimiter=";")
-labels = csv_file['Label']
+img_number = 0
+img_list = []
 
-###### Go to first unlabeled
-# find first element of labels which is empty and save corresponding row index
-# use index to get corresponding color from colormap
-
-# use function findImages(color) to get a list of images with this color
-# create an image with this color
-# show this image
-# open first image from the returned list
-# user can type n for next image and p previous image, c for color image, l for label input
-# add label input to labels
-# csv_file.set_value(row)['Label'] = l
-# csv_file.to_csv("LabelColorMapping.csv", sep=';')
-
-for infile in glob.glob("../LabeledImages/*gt.png"):
-    # file, ext = os.path.splitext(infile)
-    img = Image.open(infile)
+def main():
+    colormap = np.loadtxt("LabelColorMapping.csv",skiprows=1,delimiter=';',usecols=(1,2,3))
+    colormap = colormap.astype(int)
     
-    img_colors = np.array(img) # Convert to NumPy array to easier access
+    global csv_file
+    csv_file = pd.read_csv("LabelColorMapping.csv",delimiter=";")
+    labels = csv_file['Label']
     
-    # Get the colour palette
-    palette = img.getpalette()
+    rows = np.where(np.array(pd.isnull(labels)))[0]
     
-    if palette == None:
-        print('Add palette: ' + file)
-        img.putpalette(original_map)
-        img.save(infile)
-    else:
+    print(str(len(rows)) + " unlabeled pixels found.")
+    
+    if len(rows) == 0:
+        print("Nothing to do.")
+        sys.exit()
+    
+    global color_index
+    color_index = rows[0]
+    color = colormap[color_index]
+    
+    global img_list
+    img_list = findImages(color_index)
+    
+    if (len(img_list) == 0):
+        print("No image with this color.")
+        sys.exit()
+    
+    global img_number
+    
+    img = Image.open(img_list[img_number])
+    color_img = Image.new('RGB',(60,60),tuple(color))
+    
+    # color_img.show()
+    global root
+    root = tkinter.Tk()
+    
+    photo_color = ImageTk.PhotoImage(color_img)
+    photo_img = ImageTk.PhotoImage(img)
+    
+    # logo = PhotoImage(file="../images/python_logo_small.gif")
+    w1 = tkinter.Label(root, image=photo_color).pack()
+    explanation = """Find label name of this color:"""
+    w2 = tkinter.Label(root, 
+               padx = 10, 
+               text=explanation).pack()
+    global w3
+    w3 = tkinter.Label(root, image=photo_img)
+    w3.pack()
+    root.bind_all('<Key>', key)
+    root.mainloop()
+
+def key(event):
+    global root
+    root.unbind_all('<Key>')
+    if event.char in ('n', 'p', 'c'):
+        global img_number
+        global img_list
         
-        # Determine the total number of colours
-        num_colours = len(palette)/3
+        if event.char == 'n':
+            img_number = img_number+1
+        elif event.char == 'p':
+            img_number = img_number-1
         
-        # Determine maximum value of the image data type
-        # max_val = float(np.iinfo(indexed.dtype).max)
+        img_path = img_list[img_number]
         
-        # Create a colour map matrix
-        map = np.array(palette).reshape(num_colours, 3) # / max_val
+        if event.char == 'c':
+            img_path = string.replace(img_path, 'gt', 'image')
         
-        if (np.array_equal(original_map,map)):
-            print('Correct: ' + file)
-        else:
-            print('False: ' + file)
+        img_number = img_number % (len(img_list)-1)
+        img = Image.open(img_list[img_number])
+        photo_img = ImageTk.PhotoImage(img)
+        global w3
+        w3.configure(image=photo_img)
+        w3.image = photo_img
+    if event.char == 'l':
+        label = simpledialog.askstring('Input', 'Label Name')
+        global csv_file
+        global color_index
+        # add label input to labels
+        csv_file.set_value(color_index,'Label',label)
+        csv_file.to_csv("LabelColorMapping.csv", sep=';',index=False)
+        root.quit()
+    root.bind_all('<Key>', key)
 
 
-def findImages(color):
+def findImages(color_index):
     out = []
-    for infile in glob.glob("../LabeledImages/*gt.png"):        
+    for infile in sorted(glob.glob("../LabeledImages/*gt.png")):
         img = Image.open(infile)
         img_colors = np.array(img)
         
-        if (color in img_colors.tolist()):
-            out.push(infile)
+        
+        if (color_index in img_colors[:,:]):
+            out.append(infile)
+    return out
+
+if __name__ == "__main__":
+    main()
